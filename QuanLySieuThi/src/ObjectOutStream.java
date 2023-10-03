8ef8676de4dc9ecfff66719f6618fb636d00c5e6
DuyVuNg2109 <vu210920003@gmail.com>
2023-10-03 22:22:52 +0700

    first commit

---
+++ QuanLySieuThi/src/ObjectOutStream.java
@@ -0,0 +1,28 @@
+
+import java.io.FileOutputStream;
+import java.util.Vector;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class ObjectOutStream {
+
+    ObjectOutStream(FileOutputStream file) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void writeObject(Vector<Vector> tableData) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void close() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
